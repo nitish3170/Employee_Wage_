0a93292a41e1568ff3853312160658dd3d905222
david <david@Nitishs-MacBook-Air-501.local>
2024-06-07 15:02:14 +0530

    To Check Employee is Present or Absent

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+    public static boolean EmployeeAttendance(){
+        double attendance=Math.random();
+        if(attendance>0.5) return true;
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+        if (EmployeeAttendance()){
+            System.out.println("Employee is present.");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
+
     }
 }
