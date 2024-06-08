8f3021f6204baca9fc8356582e950c1298385c09
david <david@Nitishs-MacBook-Air-501.local>
2024-06-08 14:59:11 +0530

    UC2 calculating daily employee wage

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,25 @@ public class Main {
         else
             System.out.println("Employee is Absent");
     }
-    
+    static void CalculateDailyWage(){
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        double empCheck = Math. floor (Math.random () * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage) ;
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        CheckAttendance();
+        CalculateDailyWage();
+        
+
     }
 }
